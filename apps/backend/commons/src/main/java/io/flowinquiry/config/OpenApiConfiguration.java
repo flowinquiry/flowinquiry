3cad878206e461eb27f87b4d816077d742314f56
Hai Nguyen <3423575+haiphucnguyen@users.noreply.github.com>
2025-06-17 16:17:46 -0700

    Update

---
+++ apps/backend/commons/src/main/java/io/flowinquiry/config/OpenApiConfiguration.java
@@ -0,0 +1,45 @@
+package io.flowinquiry.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/** Configuration for OpenAPI documentation. */
+@Configuration
+public class OpenApiConfiguration {
+
+    /**
+     * OpenAPI configuration bean.
+     *
+     * @return the OpenAPI configuration
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Flow Inquiry API")
+                                .description("Flow Inquiry REST API Documentation")
+                                .version("1.0.0")
+                                .license(
+                                        new License()
+                                                .name("AGPLv3")
+                                                .url("https://opensource.org/licenses/MIT")))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearer-jwt",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                                .in(SecurityScheme.In.HEADER)
+                                                .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+}
