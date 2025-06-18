8fdcc020f4132f7e7059ed8f4e4bc3cf0801e25d
Hai Nguyen <3423575+haiphucnguyen@users.noreply.github.com>
2025-06-17 19:53:32 -0700

    Update

--- apps/backend/commons/src/main/java/io/flowinquiry/config/OpenApiConfiguration.java
+++ apps/backend/commons/src/main/java/io/flowinquiry/config/OpenApiConfiguration.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,10 +28,19 @@ public class OpenApiConfiguration {
                                 .title("Flow Inquiry API")
                                 .description("Flow Inquiry REST API Documentation")
                                 .version("1.0.0")
+                                .extensions(
+                                        Map.of(
+                                                "x-logo",
+                                                Map.of(
+                                                        "url",
+                                                                "https://docs.flowinquiry.io/images/logo.png",
+                                                        "backgroundColor", "#FFFFFF",
+                                                        "altText", "FlowInquiry logo")))
                                 .license(
                                         new License()
                                                 .name("AGPLv3")
-                                                .url("https://opensource.org/licenses/MIT")))
+                                                .url("https://opensource.org/license/agpl-v3")))
+                .addServersItem(new Server().url("https://flowinquiry.io"))
                 .components(
                         new Components()
                                 .addSecuritySchemes(
