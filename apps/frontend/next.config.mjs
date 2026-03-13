73b8ecd547b220fc4ea04df423dd6602243a6639
Hai Nguyen <3423575+haiphucnguyen@users.noreply.github.com>
2026-03-13 14:31:05 -0700

    Feature/fix frontend build (#305)

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from "next-intl/plugin";
 
+const backEndUrl = process.env.BACK_END_URL || "http://localhost:8080";
+
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
@@ -8,15 +10,10 @@ const nextConfig = {
       fallback: [
         {
           source: "/api/:path*",
-          destination: `${process.env.BACK_END_URL}/api/:path*`,
+          destination: `${backEndUrl}/api/:path*`,
         },
       ],
     };
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
   output: "standalone",
 };
