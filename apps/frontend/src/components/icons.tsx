4aad1b962d481bd70ac2a7af0f5e6540998815f5
Hai Nguyen <3423575+haiphucnguyen@users.noreply.github.com>
2026-07-04 21:09:09 -0700

    Update

--- apps/frontend/src/components/icons.tsx
+++ apps/frontend/src/components/icons.tsx
@@ -1,4 +1,4 @@
-export type IconProps = React.ComponentPropsWithoutRef<"svg">;
+export type IconProps = React.SVGAttributes<SVGSVGElement>;
 
 export const Icons = {
   spinner: (props: IconProps) => (
